a6051985719247d241e7de5ec134b35e4bb42cad
Cas9Diego <DaCastro@outlook.com>
2022-08-02 17:56:38 -0500

    Second commit

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -7,12 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]() //This variable is defined outside of the below class, because we want it to exist as long as the viewcontroller exists. çotherwise, it would be destroyed whemn viewDidLoad finishes
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fm = FileManager.default //Filemanager.default is a built-in system type that let's us work with a file system
+        let path = Bundle.main.resourcePath! //This constant sets the resoure path of our apps bundle. A bundle is a directory containing our compiled program and all our assets, so this constant says "Tell me where I can find all the images that I added". The EXCLAMATION MARK is necessary because our main bundle may not have a resource path, because some app bundles dont have a resource path. However, iOS main bundles always have a resource path, so it is ok to do forced unwrapping
+        let items = try! fm.contentsOfDirectory(atPath: path) // this variable is set to the content of the directory at a path, which path? the one we defined before. Basicaly, we say "USe the filemanager to read the documents at a certain path, the path we signaled".
+        
+        for item in items {
+            if item.hasPrefix ("nssl") {
+                pictures.append(item)
+            }
+        }
+        print (pictures)
+        
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //This table has only one section (default) and pictures.count is the number of rows
+        return pictures.count
+    } //The override key means that this method is changing the behavious ir inherits from its parent class
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //The first parametert indicates that this method is called everytime a cell is created.
+        //indexPath contains a number for sections and one for rows, since there is only one section here, that one is ignored.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath) // The cell is created by dequeing and recycling cells from the table.
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell //Sets a text label to each cell.
+        
+    } //This method specifies what each row should look like
 
 
 }
